49c76d9f80f66915f474a97eb90b008c3ac6969f
omkar-4 <omkar.pro04@gmail.com>
2024-05-28 20:39:49 +0530

    Navbar finished along with Button

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+    <main className='h-screen w-full bg-[#000000] text-white'>
+    <Navbar />
+    <div className="pagetry w-full h-[400%] bg-[#000]"></div>  
+    </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BsArrowReturnRight } from "react-icons/bs";
+
+function Button() {
+  return (
+    <>
+    <div className='min-w-40 px-5 py-1.5 bg-zinc-100 text-black rounded-full flex gap-4 items-center'>
+        <span className='text-zinc-700 tracking-tight align-middle items-center justify-center flex font-normal text-md'>Start a Project</span>
+        <span className='text-[12px] translate-y-0.5'><BsArrowReturnRight /></span>
+    </div>
+    </>
+  )
+}
+
+export default Button
