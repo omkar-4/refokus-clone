46d970115a1bab6aaf92a0067ea1c11fc25c1ac3
omkar-4 <omkar.pro04@gmail.com>
2024-05-28 21:24:30 +0530

    updated readme, changed favicon, and lil bit more refining

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import { BsArrowReturnRight } from "react-icons/bs";
 function Button() {
   return (
     <>
-    <div className='min-w-40 px-5 py-1.5 bg-zinc-100 text-black rounded-full flex gap-4 items-center'>
+    <div className='min-w-40 px-5 py-1.5 bg-zinc-100 text-black rounded-full flex justify-between gap-4 items-center'>
         <span className='text-zinc-700 tracking-tight align-middle items-center justify-center flex font-normal text-md'>Start a Project</span>
         <span className='text-[12px] translate-y-0.5'><BsArrowReturnRight /></span>
     </div>
