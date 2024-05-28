f84afe492bcf4ff883c69e953d07db797e102718
omkar-4 <omkar.pro04@gmail.com>
2024-05-28 22:14:24 +0530

    created work component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Work from './components/Work'
 
 function App() {
   return (
     <>
     <main className='h-screen w-full bg-[#000000] text-white'>
     <Navbar />
-    <div className="pagetry w-full h-[400%] bg-[#000]"></div>  
+    <Work/>
+    {/* <div className="pagetry w-full h-[400%] bg-[#000]"></div> */}
     </main>
     </>
   )
---
+++ src/components/Work.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Work() {
+  return (
+    <>
+    <div className='w-full'>
+        <div className='max-w-screen-xl mx-auto'>
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Work
