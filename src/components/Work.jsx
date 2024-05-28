825ae6c933c0f88abc153cfd1c934c8c2cf7edd8
omkar-4 <omkar.pro04@gmail.com>
2024-05-28 22:41:12 +0530

    work.jsx updated to almost perfection"

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,7 +5,8 @@ function Work() {
     <>
     <div className='w-full'>
         <div className='max-w-screen-xl mx-auto'>
-            
+            <h1 className='text-[37vw] font-medium leading-none text-center overflow-hidden'>work</h1>
+            <h1 className='text-[#919191] text-center text-lg'>Web Design, Webflow Development, Creative Development</h1>
         </div>
     </div>
     </>
